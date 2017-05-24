0904052ebf1b54a5f11e75ed8cfba8a0d8cdc8a7
gavin <gx199492@qq.com>
2017-05-25 02:09:52 +0800

    加入了简单的post实验和https

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
-
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    res.render('index', { title: 'Express' });
 });
-
+router.get('/index_image', function(req, res, next) {
+	console.log(req.query);
+    res.send(req.query);
+});
 module.exports = router;
